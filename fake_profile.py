31469f829652d25f87eddb6445095f42cc29ebfb
Arnaud B <bloomfate@gmail.com>
2023-03-16 01:49:36 +0100

    update_fakeprofile_fix

--- fake_profile.py
+++ fake_profile.py
@@ -1,10 +1,10 @@
 import random
 import requests
 
-# Make a request to the randomuser API to get a random profile
+# Request to the randomuser API to get a random profile
 response = requests.get("https://randomuser.me/api/").json()
 
-# Extract the necessary information from the response data
+# Extracts the necessary information from the response data
 gender = response["results"][0]["gender"]
 age = response["results"][0]["dob"]["age"]
 first_name = response["results"][0]["name"]["first"]
@@ -16,12 +16,12 @@ address = response["results"][0]["location"]["street"]["name"] + " " + str(respo
 postcode = response["results"][0]["location"]["postcode"]
 city = response["results"][0]["location"]["city"]
 
-# Generate a random email address
+# Generates a random email address
 username = first_name.lower() + last_name.lower() + str(random.randint(100, 999))
 domain = random.choice(["gmail.com", "yahoo.com", "hotmail.com", "outlook.com"])
 email_address = f"{username}@{domain}"
 
-# Print the fake profile information
+# Prints the fake profile information in terminal
 print("Name: " + first_name + " " + last_name)
 print("Age: " + str(age))
 print("Gender: " + gender)
@@ -29,7 +29,7 @@ print("Country: " + country)
 print("Email: " + email_address)
 print("Address: " + address + ", " + str(postcode) + " " + city)
 
-# output fake profile information to a txt file
+# Outputs fake profile information to a txt file
 with open(f"{first_name}_{last_name}_profile.txt", "w") as f:
     f.write("Name: " + first_name + " " + last_name + "\n"
             " Age: " + str(age) + "\n"
@@ -38,7 +38,7 @@ with open(f"{first_name}_{last_name}_profile.txt", "w") as f:
             "Email: " + email_address + "\n"
             "Address: " + address + ", " + str(postcode) + " " + city)
 
-# Download the profile picture and save it to a file
+# Downloads the profile picture and save it to a jpg file
 response = requests.get(picture_url)
 with open(f"{first_name}_{last_name}_pic.jpg", "wb") as f:
     f.write(response.content)
