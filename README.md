16c0e134bea75d895f22fc5e7bd910eb520bdcef
Arnaud B <bloomfate@gmail.com>
2023-03-16 01:46:54 +0100

    update_fakeprofile

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## Osint scripts
-- exif_img_file.py : Extract metadata from image
-- fake_profile.py : Generate fake profile info from randomized EU locale
-- txt_from_pdf.py : Extract text from a pdf file (all pages)
-- txt_from_img.py : Extract text from image (requires Tesseract OCR)
-- youtube_dl.py : Download youtube video
+- exif_img_file.py : Extracts metadata from image
+- fake_profile.py : Generates a fake profile info and picture, outputs txt file (with profile details) and image file with the name of the fake profile.
+- txt_from_pdf.py : Extracts text from a pdf file (all pages)
+- txt_from_img.py : Extracts text from image (requires Tesseract OCR)
+- youtube_dl.py : Downloads youtube video from a link
 
-Most scripts open a file dialog to select the file. For multiple files, simply replace "filedialog.askopenfilename()" with "filedialog.askdirectory()" and loop over the files.
+Extracting scripts open a file dialog to select the file. For multiple files, simply replace "filedialog.askopenfilename()" with "filedialog.askdirectory()" and loop over the files.
--- fake_profile.py
+++ fake_profile.py
@@ -1,10 +1,44 @@
-from faker import Faker
 import random
+import requests
 
-# Create object with randomized locale
-locale = random.choice(['it_IT','en_GB','fr_FR','es_ES','de_DE','pl_PL','nl_NL'])
-fake = Faker(locale)
+# Make a request to the randomuser API to get a random profile
+response = requests.get("https://randomuser.me/api/").json()
 
-# generate fake profile
-print({"bdate":fake.date_of_birth(minimum_age=18,maximum_age=80),"name":fake.name_nonbinary(),"email":fake.email(),
-           "address":fake.street_address(),"city":fake.city(),"phone":fake.phone_number(),'job':fake.job()})
+# Extract the necessary information from the response data
+gender = response["results"][0]["gender"]
+age = response["results"][0]["dob"]["age"]
+first_name = response["results"][0]["name"]["first"]
+last_name = response["results"][0]["name"]["last"]
+email = response["results"][0]["email"]
+picture_url = response["results"][0]["picture"]["large"]
+country = response["results"][0]["location"]["country"]
+address = response["results"][0]["location"]["street"]["name"] + " " + str(response["results"][0]["location"]["street"]["number"])
+postcode = response["results"][0]["location"]["postcode"]
+city = response["results"][0]["location"]["city"]
+
+# Generate a random email address
+username = first_name.lower() + last_name.lower() + str(random.randint(100, 999))
+domain = random.choice(["gmail.com", "yahoo.com", "hotmail.com", "outlook.com"])
+email_address = f"{username}@{domain}"
+
+# Print the fake profile information
+print("Name: " + first_name + " " + last_name)
+print("Age: " + str(age))
+print("Gender: " + gender)
+print("Country: " + country)
+print("Email: " + email_address)
+print("Address: " + address + ", " + str(postcode) + " " + city)
+
+# output fake profile information to a txt file
+with open(f"{first_name}_{last_name}_profile.txt", "w") as f:
+    f.write("Name: " + first_name + " " + last_name + "\n"
+            " Age: " + str(age) + "\n"
+            "Gender: " + gender + "\n"
+            "Country: " + country + "\n"
+            "Email: " + email_address + "\n"
+            "Address: " + address + ", " + str(postcode) + " " + city)
+
+# Download the profile picture and save it to a file
+response = requests.get(picture_url)
+with open(f"{first_name}_{last_name}_pic.jpg", "wb") as f:
+    f.write(response.content)
